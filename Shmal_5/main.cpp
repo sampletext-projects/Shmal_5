83020df24648343ddbdb780c3466431c2145cefe
sampletext32 <sampletext32@bk.ru>
2020-04-17 20:19:59 +0300

    Add project

---
+++ Shmal_5/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	int matr[3][3];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "Matr[" << i << "][" << j << "] = ";
+			cin >> matr[i][j];
+		}
+	}
+
+	cout << "\nВы ввели матрицу\n";
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << setw(3) << matr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	int n;
+	cout << "Введите n: ";
+	cin >> n;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		matr[n][i] = 0;
+	}
+	
+	
+	cout << "\nРезультирующая матрица\n";
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << setw(3) << matr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	system("pause");
+
+	return 0;
+}
